11a3f2162cd0bfc2e9c1e7f575febf3935ad92fc
gradiuscypher <gradiuscypher@gmail.com>
2016-03-31 16:33:05 -0700

    logging INFO to a file if set

--- gradiusbot.py
+++ gradiusbot.py
@@ -109,6 +109,13 @@ def main_task(config_file):
     if config.getboolean("BotSettings", "debug"):
         logging.basicConfig(level=logging.DEBUG)
 
+    if config.getboolean("BotSettings", "logfile"):
+        logger = logging.getLogger('discord')
+        logger.basicConfig(level=logging.INFO)
+        handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+        handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+        logger.addHandler(handler)
+
     plugins.load_plugins(config)
 
     try:
