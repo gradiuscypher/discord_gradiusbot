0e2d1717f99879f0e3e855b5ed3f275f174f4eb7
gradiuscypher <gradiuscypher@gmail.com>
2016-05-14 17:31:58 -0700

    updating help text for public help

--- public_plugins/help.py
+++ public_plugins/help.py
@@ -5,21 +5,12 @@ print("[Public Plugin] <help.py>: This plugin helps you with commands.")
 
 help_message = """
 
-__**Available Commands**__:
+__**Basic Commands**__:
 
-!help - prints this message.
-
-!namecolor *[color]* - grants your user a different color name. Use it without a color to see a list of colors.
-                    If your desired color does not exist, speak to a chat admin.
-
-                    Can also use !namecolor random to pick a random color.
+**!help** - prints this message.
 """
 
 
 @asyncio.coroutine
 def action(message, client, config):
-    split_content = message.content.split()
-    bot_channel = config.get("BotSettings", "bot_channel")
-    if len(split_content) > 0:
-        if split_content[0] == "!help" and message.channel.name == bot_channel:
-            yield from client.send_message(message.author, help_message)
+    pass
