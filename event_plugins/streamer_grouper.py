fbdc70205fccbfd02e3e69926169f08f9a2ba675
gradiuscypher <gradiuscypher@gmail.com>
2017-03-13 22:57:56 -0700

    adding streamer_grouper.py

--- event_plugins/streamer_grouper.py
+++ event_plugins/streamer_grouper.py
@@ -1,19 +1,11 @@
 import asyncio
 import discord
 
-print("[Event Plugin] <example_event_plugin.py>: This plugin shows you how to use events.")
+print("[Event Plugin] <streamer_grouper.py>: This plugin places streaming users into a specific group.")
 
 
 @asyncio.coroutine
 def action(message, client, config, event_type, object_after=None):
-
-    if event_type == "delete":
-        yield from client.send_message(message.channel, "Deleted: " + ' '.join(message.attachments))
-        print(repr(message.attachments))
-    if event_type == "edit":
-        yield from client.send_message(message.channel, "Edited:\n " + "- " + message.content +
-                                       "\n+ " + object_after.content)
-
     if event_type == 'member_update':
         role = discord.utils.get(message.server.roles, name="streaming")
         is_streaming = discord.utils.get(message.roles, name="streaming")
