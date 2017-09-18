dfac6c06b06b4283c020675d859a8bda618c3e85
gradiuscypher <gradiuscypher@gmail.com>
2017-09-17 22:55:03 -0700

    adding some work on the discord side

--- public_plugins/lol_tournaments.py
+++ public_plugins/lol_tournaments.py
@@ -1,6 +1,9 @@
 # TODO: Complete tournament workflow:
 # TODO: tournament_start, tournament_join, tournament_end, game_start, game_join, game_end, game_stats
+# TODO: Use reactions rather than chat commands to join the game
 import asyncio
+from libs.custom_games.libs.tournament_libs import TournamentManager
+from libs import permissions
 
 print("[Public Plugin] <lol_tournaments.py>: This plugin manages LoL custom tournaments.")
 
@@ -8,11 +11,23 @@ help_message = """
 __**!tournament help** (Public Channels Only)__
 Manages LoL Custom Tournaments
 
-!tournament seasonstart - start a new season [requires permission]
-!tournament join season - join the current season
-!tournament join game - join the current game being created
-!tournament stats - show the stats and leaderboard.
+!season start <SEASON_NAME> - start a new season [requires permission]
+!season end - end a season [requires permission]
+!season get - get season details [requires permission]
 """
+
+manager = TournamentManager()
+
+
+def start_tournament(name):
+    started = manager.start_tournament(name)
+    return started
+
+
+def tournament_end():
+    tournament = manager.get_active_tournaments()[0]
+    ended = tournament.complete_tournament()
+    return ended
 
 
 @asyncio.coroutine
@@ -22,16 +37,33 @@ def action(message, client, config):
     if split_content[0] == "!tournament":
 
         # Season management - Admin Only
-        if split_content[1] == "seasonstart":
-            pass
-        if split_content[1] == "seasonend":
-            pass
+        if (permissions.has_admin(message.author)) is not None:
+            if len(split_content) == 3:
+                if split_content[1] == "start":
+                    name = split_content[2]
+                    started = start_tournament(name)
+                    if started:
+                        start_message = f'Started {name} Tournament successfully.'
+                        yield from client.send_message(message.channel, start_message)
+                    else:
+                        fail_message = "Tournament failed to start, there may already be one running."
+                        yield from client.send_message(message.channel, fail_message)
+            if split_content[1] == "end":
+                stopped = tournament_end()
 
+                if stopped:
+                    start_message = f'Ended Tournament successfully.'
+                    yield from client.send_message(message.channel, start_message)
+                else:
+                    fail_message = "Tournament failed to end."
+                    yield from client.send_message(message.channel, fail_message)
+
+            if split_content[1] == "list":
+                tournament = manager.get_active_tournaments()[0]
+                tourney_message = f"The current active tournament is {tournament.name}."
+                yield from client.send_message(message.channel, tourney_message)
+
+    if split_content[0] == "!game":
         # Game management - All Users
         if split_content[1] == "join":
-            if split_content[2] == "season":
-                pass
-            if split_content[2] == "game":
-                pass
-        if split_content[1] == "stats":
-            pass
+            yield from client.send_message(message.channel, "JOIN")
