0a0941407f34baf651888dc725d1abbbee7a82fc
gradiuscypher <gradiuscypher@gmail.com>
2017-09-22 19:58:30 -0700

    removing old code that will no longer be maintained

--- libs/items/basic.py
+++
@@ -1,7 +0,0 @@
-import random
-import asyncio
-
-
-def dice_action(client=None, message=None, config=None):
-    roll = random.randint(1, 20)
-    yield from client.send_message(message.channel, "Dice roll: {}".format(roll))
