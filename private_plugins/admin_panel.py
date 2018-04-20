2d9331aac689fa29608ac0e956d133eacf8eb8f6
gradiuscypher <gradiuscypher@gmail.com>
2018-04-20 14:25:06 -0700

    adding getuserinfo to admin panel

--- private_plugins/admin_panel.py
+++ private_plugins/admin_panel.py
@@ -85,3 +85,29 @@ async def action(message, client, config):
                 result_string += '```'
                 await client.send_message(message.channel, result_string)
 
+            if split_content[1] == 'getuserinfo' and len(split_content) == 3:
+                user_id = split_content[2]
+                found_user = False
+
+                user_embed = Embed(title="Discord User", color=Color.green())
+                user_embed.add_field(name="User ID", value=user_id, inline=False)
+                server_name_string = ''
+                user_object = None
+
+                for server in client.servers:
+                    user = server.get_member(user_id)
+
+                    if user:
+                        found_user = True
+                        server_name_string += server.name + "\n"
+                        user_object = user
+
+                if found_user:
+                    user_embed.add_field(name="In Servers", value=server_name_string)
+                    user_embed.add_field(name="User Name", value=user_object.name + "#" + str(user_object.discriminator), inline=True)
+                    user_embed.set_thumbnail(url=user_object.avatar_url)
+
+                    await client.send_message(message.channel, embed=user_embed)
+                else:
+                    fail_embed = Embed(title="Discord User", color=Color.red(), description="User was not found on any of my servers.")
+                    await client.send_message(message.channel, embed=fail_embed)
