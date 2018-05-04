75e64669d48e5236f954decc2b3dc5b32f948d21
gradiuscypher <gradiuscypher@gmail.com>
2018-05-03 20:06:14 -0700

    Partially complete version of the streamer grouper re-write

--- event_plugins/member/streamer_grouper/streamer_grouper.py
+++ event_plugins/member/streamer_grouper/streamer_grouper.py
@@ -18,19 +18,40 @@ async def action(**kwargs):
         after_activity = member_after.activity
         streaming_before = utils.get(member_before.roles, name='streaming')
         streaming_after = utils.get(member_after.roles, name='streaming')
+        guild = member_after.guild
+        debug_channel = utils.get(guild.channels, name='debug')
+        streaming_role = utils.get(guild.roles, name='streaming')
 
-        if after_activity and after_activity.type.name == 'listening' and streaming_after is None:
-            # the user is streaming
-            guild = member_after.guild
-            debug_channel = utils.get(guild.channels, name='debug')
-            streaming_role = utils.get(guild.roles, name='streaming')
-            await member_after.add_roles(streaming_role, reason='{} started streaming.'.format(member_after.name))
-            await debug_channel.send('{} is streaming!'.format(member_after.name))
+        # Debug
+        print("Member Update!\nbefore:{}\nafter:{}".format(before_activity, after_activity))
 
-        if streaming_before and before_activity and before_activity.type.name == 'listening' and after_activity != 'listening':
-            # the user is not streaming
-            guild = member_after.guild
-            debug_channel = utils.get(guild.channels, name='debug')
-            streaming_role = utils.get(guild.roles, name='streaming')
-            await member_after.remove_roles(streaming_role, reason='{} stopped streaming.'.format(member_after.name))
-            await debug_channel.send('{} is NOT streaming!'.format(member_after.name))
+        # If the user is going from no activity to streaming
+        if before_activity is None:
+            if after_activity and after_activity.type.name == 'listening' and streaming_after is None:
+                print('User is streaming and is not in the streaming group')
+                await member_after.add_roles(streaming_role, reason='{} started streaming.'.format(member_after.name))
+                await debug_channel.send('{} is streaming!'.format(member_after.name))
+                return
+
+        # If the user is going from another activity to streaming and isn't in the streamer group
+        if before_activity and not streaming_before:
+            if after_activity and after_activity.type.name == 'listening' and streaming_after is None:
+                print("User is streaming, and was doing something else before, and is not in the streamers group")
+                await member_after.add_roles(streaming_role, reason='{} started streaming.'.format(member_after.name))
+                await debug_channel.send('{} is streaming!'.format(member_after.name))
+                return
+
+        # If the user was streaming before
+        if streaming_before:
+            # If the user is doing something else that's not streaming
+            if after_activity and after_activity.type.name != 'listening':
+                print("The User was streaming before and now is no longer streaming but is doing something else")
+                await member_after.remove_roles(streaming_role, reason='{} stopped streaming.'.format(member_after.name))
+                await debug_channel.send('{} is NOT streaming!'.format(member_after.name))
+                return
+            # If the user isn't doing anything else
+            if not after_activity:
+                print("The User was streaming before and now is no longer streaming or doing anything")
+                await member_after.remove_roles(streaming_role, reason='{} stopped streaming.'.format(member_after.name))
+                await debug_channel.send('{} is NOT streaming!'.format(member_after.name))
+                return
