b3f0099eafe5a50d209bf356849986660fdbcf1f
gradiuscypher <gradiuscypher@gmail.com>
2018-05-03 21:21:28 -0700

    Streamer grouper changes and debug stuff

--- event_plugins/member/streamer_grouper/streamer_grouper.py
+++ event_plugins/member/streamer_grouper/streamer_grouper.py
@@ -10,6 +10,9 @@ logger.info("[Member Event Plugin] <streamer_grouper.py>: This plugin groups peo
 @asyncio.coroutine
 async def action(**kwargs):
     event_type = kwargs['event_type']
+    config = kwargs['config']
+    debug_channel_name = config.get('gradiusbot', 'debug_channel')
+    debug = config.getboolean('gradiusbot', 'debug')
 
     if event_type == 'member.update':
         member_before = kwargs['before']
@@ -19,39 +22,36 @@ async def action(**kwargs):
         streaming_before = utils.get(member_before.roles, name='streaming')
         streaming_after = utils.get(member_after.roles, name='streaming')
         guild = member_after.guild
-        debug_channel = utils.get(guild.channels, name='debug')
+        debug_channel = utils.get(guild.channels, name=debug_channel_name)
         streaming_role = utils.get(guild.roles, name='streaming')
-
-        # Debug
-        print("Member Update!\nbefore:{}\nafter:{}".format(before_activity, after_activity))
 
         # If the user is going from no activity to streaming
         if before_activity is None:
-            if after_activity and after_activity.type.name == 'listening' and streaming_after is None:
-                print('User is streaming and is not in the streaming group')
+            if after_activity and after_activity.type.name == 'streaming' and streaming_after is None:
                 await member_after.add_roles(streaming_role, reason='{} started streaming.'.format(member_after.name))
-                await debug_channel.send('{} is streaming!'.format(member_after.name))
+                if debug:
+                    await debug_channel.send('{} is streaming!'.format(member_after.name))
                 return
 
         # If the user is going from another activity to streaming and isn't in the streamer group
         if before_activity and not streaming_before:
-            if after_activity and after_activity.type.name == 'listening' and streaming_after is None:
-                print("User is streaming, and was doing something else before, and is not in the streamers group")
+            if after_activity and after_activity.type.name == 'streaming' and streaming_after is None:
                 await member_after.add_roles(streaming_role, reason='{} started streaming.'.format(member_after.name))
-                await debug_channel.send('{} is streaming!'.format(member_after.name))
+                if debug:
+                    await debug_channel.send('{} is streaming!'.format(member_after.name))
                 return
 
         # If the user was streaming before
         if streaming_before:
             # If the user is doing something else that's not streaming
-            if after_activity and after_activity.type.name != 'listening':
-                print("The User was streaming before and now is no longer streaming but is doing something else")
+            if after_activity and after_activity.type.name != 'streaming':
                 await member_after.remove_roles(streaming_role, reason='{} stopped streaming.'.format(member_after.name))
-                await debug_channel.send('{} is NOT streaming!'.format(member_after.name))
+                if debug:
+                    await debug_channel.send('{} is NOT streaming!'.format(member_after.name))
                 return
             # If the user isn't doing anything else
             if not after_activity:
-                print("The User was streaming before and now is no longer streaming or doing anything")
                 await member_after.remove_roles(streaming_role, reason='{} stopped streaming.'.format(member_after.name))
-                await debug_channel.send('{} is NOT streaming!'.format(member_after.name))
+                if debug:
+                    await debug_channel.send('{} is NOT streaming!'.format(member_after.name))
                 return
