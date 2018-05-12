9fae88b5a62ab5467b1bf22fab2731e5e8c56099
gradiuscypher <gradiuscypher@gmail.com>
2018-05-12 15:05:34 -0700

    removing old reference files

--- old/public_plugins/delete_commands.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-
-print("[Public Plugin] <delete_commands.py>: This plugin deletes messages from a channel if it starts with !.")
-
-
-@asyncio.coroutine
-def action(message, client, config):
-    if message.content.startswith("!"):
-        yield from client.delete_message(message)
--- old/public_plugins/echo.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-
-print("[Public Plugin] <echo.py>: This plugin echoes stuff to a public channel.")
-
-
-@asyncio.coroutine
-def action(message, client, config):
-    yield from client.send_message(message.channel, message.content)
