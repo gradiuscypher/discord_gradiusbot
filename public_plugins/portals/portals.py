65038873896c518d3bde6df97b7f126ff02546e4
gradiuscypher <gradiuscypher@gmail.com>
2018-10-17 10:34:36 -0700

    im hardcoding things fite me

--- public_plugins/portals/portals.py
+++ public_plugins/portals/portals.py
@@ -15,14 +15,14 @@ async def action(**kwargs):
     client = kwargs['client']
 
     # if the portal is blue, store a key:value of user:portal content
-    if '<:portal_blue:502160330911121428>' in message.content:
-        remaining_content = message.content.replace('<:portal_blue:502160330911121428>', '')
+    if '<:portal_blue:502170507198857226>' in message.content:
+        remaining_content = message.content.replace('<:portal_blue:502170507198857226>', '')
         portal_dict[message.author.id] = {"content": remaining_content, "message": message}
 
     # if the portal is red, check to see if the user has any blue portals, and move the content from there, to here.
-    if '<:portal_red:502160345293520907>' in message.content:
+    if '<:portal_orange:502170488680874004>' in message.content:
         if message.author.id in portal_dict.keys() and portal_dict[message.author.id]:
-            await message.channel.send('<@' + str(message.author.id) + '>: <:portal_red:502160345293520907>' + portal_dict[message.author.id]['content'])
+            await message.channel.send('<@' + str(message.author.id) + '>: <:portal_orange:502170488680874004>' + portal_dict[message.author.id]['content'])
             await portal_dict[message.author.id]['message'].delete()
             await message.delete()
             portal_dict[message.author.id] = None
