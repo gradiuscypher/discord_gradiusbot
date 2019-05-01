43c31e8c61b465d34a6ef4419a3f0846ecb878b5
gradiuscypher <gradiuscypher@gmail.com>
2019-04-30 18:19:20 -0700

    events now also ban like sched. tasks

--- event_plugins/member/banpool_member_events/banpool_member_events.py
+++ event_plugins/member/banpool_member_events/banpool_member_events.py
@@ -2,10 +2,12 @@ import asyncio
 import discord
 import logging
 import traceback
-from libs import banpool
 from discord import Embed, Color
 
+from libs import banpool, banpool_configuration
+
 banpool_manager = banpool.BanPoolManager()
+banpool_config = banpool_configuration.BanpoolConfigManager()
 
 # Setup Logging
 logger = logging.getLogger('banpool_manager')
@@ -39,13 +41,23 @@ async def action(**kwargs):
                 banpool_manager.add_user_to_exceptions(user.id, guild.id)
 
                 unban_embed = Embed(title="User Exception Added", color=Color.orange())
+                unban_embed.add_field(name="User Name", value=user.name + "#" + str(user.discriminator), inline=False)
                 unban_embed.add_field(name="Server ID", value=guild.id, inline=True)
                 unban_embed.add_field(name="User ID", value=user.id, inline=True)
-                unban_embed.add_field(name="User Name", value=user.name + "#" + str(user.discriminator), inline=True)
-                unban_embed.set_thumbnail(url=user.avatar_url)
                 unban_embed.set_footer(icon_url=guild.icon_url, text=guild.name)
 
                 await admin_chan.send(embed=unban_embed)
+
+                # check if the server has an announce channel set, if so, announce the ban
+                announce_chan_id = banpool_config.get_announce_chan(guild.id)
+
+                if announce_chan_id:
+                    announce_chan = discord.utils.get(guild.channels, id=announce_chan_id)
+                    announce_embed = Embed(title="User Exception Added", color=Color.orange())
+                    announce_embed.add_field(name="User Name", value=user.name + "#" + str(user.discriminator), inline=True)
+                    announce_embed.add_field(name="User Profile", value=f"<@{user.id}>", inline=False)
+                    announce_embed.add_field(name="User ID", value=user.id, inline=False)
+                    await announce_chan.send(embed=announce_embed)
         except:
             logger.error(traceback.format_exc())
 
@@ -62,36 +74,51 @@ async def action(**kwargs):
         try:
             result = banpool_manager.is_user_banned(member.id)
 
-            # The member is banpooled and we need to ban them
+            # The member is banpooled and we might need to ban them
             if result[1]:
-                banpool_name = result[0]
-                reason = result[2]
-                logger.debug('member_join was caught for banpool ID: {}'.format(member.id))
-                user_id = member.id
-                user_name = member.name
-                user_discriminator = member.discriminator
-                user_avatar_url = member.avatar_url
-                server_id = member.guild.id
-                server_icon_url = member.guild.icon_url
-                server_name = member.guild.name
+                # check if the guild is subscribed to the banpool
+                is_subscribed = banpool_config.is_guild_subscribed(guild.id, result[0])
 
-                is_exception = banpool_manager.is_user_in_exceptions(user_id, server_id)
+                if is_subscribed:
+                    banpool_name = result[0]
+                    reason = result[2]
+                    logger.debug('member_join was caught for banpool ID: {}'.format(member.id))
+                    user_id = member.id
+                    user_name = member.name
+                    user_discriminator = member.discriminator
+                    server_id = member.guild.id
 
-                bot_perms = admin_chan.permissions_for(member.guild.me)
+                    is_exception = banpool_manager.is_user_in_exceptions(user_id, server_id)
 
-                if not is_exception and bot_perms.ban_members:
-                    logger.debug('member joined is in the banpool and has no exceptions: {}'.format(member.id))
-                    banpool_manager.set_last_knowns(user_id, user_name, user_discriminator)
-                    ban_embed = Embed(title="User Banned on Join", color=Color.green())
-                    ban_embed.add_field(name="Server ID", value=server_id, inline=True)
-                    ban_embed.add_field(name="User ID", value=user_id, inline=True)
-                    ban_embed.add_field(name="User Name", value=user_name + "#" + str(user_discriminator), inline=False)
-                    ban_embed.add_field(name="Ban Reason", value=reason, inline=False)
-                    ban_embed.set_thumbnail(url=user_avatar_url)
-                    ban_embed.set_footer(icon_url=server_icon_url, text=server_name)
+                    bot_perms = admin_chan.permissions_for(member.guild.me)
 
-                    await guild.ban(member, reason="Banpool Bot [{}] - {}".format(banpool_name, reason))
-                    await admin_chan.send(embed=ban_embed)
+                    if not is_exception and bot_perms.ban_members:
+                        logger.debug('member joined is in the banpool and has no exceptions: {}'.format(member.id))
+                        banpool_manager.set_last_knowns(user_id, user_name, user_discriminator)
+
+                        ban_embed = Embed(title="User Banned on Join", color=Color.green())
+                        ban_embed.add_field(name="User Name", value=user_name + "#" + str(user_discriminator), inline=False)
+                        ban_embed.add_field(name="Server ID", value=guild.id, inline=True)
+                        ban_embed.add_field(name="User ID", value=user_id, inline=True)
+                        ban_embed.add_field(name="Banpool Name", value=banpool_name, inline=False)
+                        ban_embed.add_field(name="Ban Reason", value=reason, inline=False)
+                        ban_embed.set_footer(icon_url=guild.icon_url, text=guild.name)
+                        await admin_chan.send(embed=ban_embed)
+
+                        # check if the server has an announce channel set, if so, announce the ban
+                        announce_chan_id = banpool_config.get_announce_chan(guild.id)
+
+                        if announce_chan_id:
+                            announce_chan = discord.utils.get(guild.channels, id=announce_chan_id)
+                            announce_embed = Embed(title="User Banned on Join", color=Color.green())
+                            announce_embed.add_field(name="User Name", value=member.name + "#" + str(member.discriminator), inline=True)
+                            announce_embed.add_field(name="Nickname", value=member.nick, inline=True)
+                            announce_embed.add_field(name="User Profile", value=f"<@{member.id}>", inline=False)
+                            announce_embed.add_field(name="User ID", value=user_id, inline=False)
+                            announce_embed.set_footer(icon_url=guild.icon_url, text="See Admin Mains for more details")
+                            await announce_chan.send(embed=announce_embed)
+
+                        await guild.ban(member, reason="Banpool Bot [{}] - {}".format(banpool_name, reason))
         except:
             logger.error(traceback.format_exc())
 
