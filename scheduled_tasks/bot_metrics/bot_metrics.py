15500f27eb817f3b1c7a2324836342c82bb0c7da
gradiuscypher <gradiuscypher@gmail.com>
2020-07-21 01:23:12 -0700

    adding wait in metrics to ensure that we're logged in

--- scheduled_tasks/bot_metrics/bot_metrics.py
+++ scheduled_tasks/bot_metrics/bot_metrics.py
@@ -11,30 +11,34 @@ logger.setLevel(logging.DEBUG)
 logger.info("[Scheduled Task] <bot_metrics.py>: Collects metrics about the bot every hour.")
 
 
-@loop(seconds=3600)
-async def collect_metrics(client, config):
-    try:
-        if client.is_ready():
-            logger.debug("Starting bot metrics collection...")
-            # collect the metrics about guilds
-            guild_names = []
-
-            for guild in client.guilds:
-                guild_names.append(guild.name)
-
-            metrics_dict = {
-                'total_guilds': len(client.guilds),
-                'guild_names': guild_names
-            }
-            logger.info("Metrics collection complete. Waiting 1 hour.", extra=metrics_dict)
-
-    except RuntimeError:
-        logger.error(traceback.format_exc())
-        exit(0)
-
-    except:
-        logger.error(traceback.format_exc())
-
-
 async def action(client, config):
-    collect_metrics.start(client, config)
+    @loop(seconds=3600)
+    async def collect_metrics():
+        try:
+            if client.is_ready():
+                logger.debug("Starting bot metrics collection...")
+                # collect the metrics about guilds
+                guild_names = []
+
+                for guild in client.guilds:
+                    guild_names.append(guild.name)
+
+                metrics_dict = {
+                    'total_guilds': len(client.guilds),
+                    'guild_names': guild_names
+                }
+                logger.info("Metrics collection complete. Waiting 1 hour.", extra=metrics_dict)
+
+        except RuntimeError:
+            logger.error(traceback.format_exc())
+            exit(0)
+
+        except:
+            logger.error(traceback.format_exc())
+
+    @collect_metrics.before_loop
+    async def before_loop():
+        logger.info("Waiting for client to log in before starting metrics task...")
+        await client.wait_until_ready()
+
+    collect_metrics.start()
