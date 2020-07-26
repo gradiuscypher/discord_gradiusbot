c8fcca2889bac07a553d7bcfdffb1561755ecefa
gradiuscypher <gradiuscypher@gmail.com>
2020-07-26 10:23:28 -0700

    working example with discord actions

--- libs/scripts/examples.py
+++ libs/scripts/examples.py
@@ -1,2 +1,5 @@
-def example_action_1():
+async def example_action_1(**kwargs):
+    message = kwargs['message']
+
     print("EXAMPLE ACTION 1")
+    await message.channel.send("THIS IS AN EXAMPLE ACTION1")
--- public_plugins/example_script_plugin/example_script_plugin.py
+++ public_plugins/example_script_plugin/example_script_plugin.py
@@ -21,4 +21,4 @@ async def action(**kwargs):
     # ref: https://stackoverflow.com/questions/3061/calling-a-function-of-a-module-by-using-its-name-a-string
     if message.content == 'run plugin':
         method = getattr(libs.scripts.examples, 'example_action_1')
-        method()
+        await method(message=message)
