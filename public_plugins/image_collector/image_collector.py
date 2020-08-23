1fb7529ad77c05665b86f2620b219025e5ff0fe8
gradiuscypher <gradiuscypher@gmail.com>
2020-08-23 16:27:11 -0700

    updating image collector

--- public_plugins/image_collector/image_collector.py
+++ public_plugins/image_collector/image_collector.py
@@ -1,3 +1,4 @@
+import json
 import logging
 from discord import Embed, Color
 from datetime import datetime
@@ -34,12 +35,14 @@ async def action(**kwargs):
 
                         # collect message attachments
                         if len(msg.attachments) > 0:
+                            attachment_count = 1
                             timestamp = msg.created_at
                             author = msg.author.id
 
                             for attachment in msg.attachments:
-                                await attachment.save(f"images/{author}-{int(timestamp.timestamp())}")
+                                await attachment.save(f"images/{author}-{int(timestamp.timestamp())}-{attachment_count}")
                                 file_count += 1
+                                attachment_count += 1
 
                         if message_count % 50 == 0:
                             status_embed = Embed(title="Image Collection Script", color=Color.orange(),
@@ -54,3 +57,22 @@ async def action(**kwargs):
                     status_embed.add_field(name="Messages processed", value=str(message_count))
                     status_embed.add_field(name="Files Saved", value=str(file_count))
                     await status_message.edit(embed=status_embed)
+
+            elif split_message[1] == 'userjson':
+                status_embed = Embed(title="Image Collection Script - User JSON", color=Color.green(),
+                                     description="User JSON collection script complete.")
+                member_dict = {}
+
+                for member in guild.members:
+                    member_dict[member.id] = {
+                        'name': member.name,
+                        'discriminator': member.discriminator,
+                        'nick': member.nick,
+                        'joined': member.joined_at.strftime('%Y-%m-%d %H:%M:%S')
+                    }
+
+                with open('users.json', 'w') as user_json:
+                    user_json.write(json.dumps(member_dict))
+
+                await message.channel.send(embed=status_embed)
+
