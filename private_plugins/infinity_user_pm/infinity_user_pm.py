ab7ee6f6bdd304b4a9e2787a2d60af36a1b370e8
gradiuscypher <gradiuscypher@gmail.com>
2020-09-14 17:02:49 -0700

    working edit workflow

--- private_plugins/infinity_user_pm/infinity_user_pm.py
+++ private_plugins/infinity_user_pm/infinity_user_pm.py
@@ -1,13 +1,11 @@
-# TODO: move all state changes into the function calls rather than in the chat commands
 # TODO: after validation move character names to the DB
+# TODO: add a lot more logging
 
-import discord
 import logging
 import re
 import requests
 import traceback
 from io import BytesIO
-from discord import Embed, Color
 from libs.infinity_management import mgmt_db
 from libs.infinity_management import screenshot_processing
 
@@ -25,6 +23,7 @@ help_msg = """**Amos Bot - Command Help**\n
 **Pilot Services**
 ```
 validate - starts the screenshot validation process
+restart - restart the entire validation process, can be used at any time
 ```
 
 **Other Commands**
@@ -47,34 +46,53 @@ async def action(**kwargs):
     if message.author.id in message_state.keys():
         if len(split_message) == 0:
             if message_state[message.author.id] == 'SEND_SCREENSHOT' and len(message.attachments) > 0:
+                """
+                The user has sent a screenshot, and we're in the SEND_SCREENSHOT state
+                """
                 profile_image = BytesIO(requests.get(message.attachments[0].url).content)
                 name_list = screenshot_processing.process_screenshot(profile_image)
                 await confirm_names(name_list, message)
 
         if len(split_message) == 1:
+            if split_message[0] == 'restart':
+                """
+                Allows the user to restart the entire process.
+                """
+                message_state[message.author.id] = 'READY'
+
+            if message_state[message.author.id] == 'READY' and split_message[0] == 'validate':
+                """
+                when a user wants to provide a validation screenshot, required for a validated pilot account
+                """
+                await validate_screenshot(message)
+
             if message_state[message.author.id] == 'VALIDATING' and split_message[0] == 'confirm':
-                await message.channel.send("Thank you for confirming, your names and screenshot will be shared with the interview channel.")
+                await message.channel.send("Thank you for validating your character names.")
                 message_state[message.author.id] = 'READY'
 
             if message_state[message.author.id] == 'EDIT' and split_message[0] == 'confirm':
-                await confirm_edit(message)
+                await confirm_edit(message, True)
+
+            if message_state[message.author.id] == 'EDIT' and split_message[0] == 'cancel':
+                await confirm_edit(message, False)
 
         if len(split_message) >= 3:
             if message_state[message.author.id] == 'VALIDATING' and split_message[0] == 'edit':
                 await edit_names(message)
 
     elif len(split_message) == 1:
+        if split_message[0] == 'help':
+            """
+            user is requesting the help documentation
+            """
+            await message.channel.send(help_msg)
+
         if split_message[0] == 'validate':
             """
             when a user wants to provide a validation screenshot, required for a validated pilot account
+            the user currently has no state value
             """
             await validate_screenshot(message)
-
-        if split_message[0] == 'help':
-            """
-            when a user wants to provide a validation screenshot, required for a validated pilot account
-            """
-            await message.channel.send(help_msg)
 
 
 async def validate_screenshot(message):
@@ -89,25 +107,39 @@ async def validate_screenshot(message):
                                "- If you run into any issues, contact gradius#8902 on Discord.\n```")
 
 
-async def confirm_edit(message):
+async def confirm_edit(message, confirm):
     """
     Fires when a user is confirming the edit of a character name.
     :param message:
+    :param confirm:
     :return:
     """
-    name_bucket = temp_name_bucket[message.author.id]
-    char_name_dict[message.author.id][name_bucket[0]] = name_bucket[1]
-    character_names = char_name_dict[message.author.id]
-    name_msg = ""
+    if confirm:
+        name_bucket = temp_name_bucket[message.author.id]
+        char_name_dict[message.author.id][name_bucket[0]] = name_bucket[1]
+        character_names = char_name_dict[message.author.id]
+        name_msg = ""
 
-    for char_number in character_names.keys():
-        name_msg += f"{char_number}) {character_names[char_number]}\n"
+        for char_number in character_names.keys():
+            name_msg += f"{char_number}) {character_names[char_number]}\n"
 
-    await message.channel.send("Thank you for editing your character name. The current names are as follows:\n"
-                               f"```\n{name_msg}\n```\n"
-                               f"If you would like to edit another name, repeat the edit command like last time, otherwise type `confirm` to confirm your names.")
+        await message.channel.send("Thank you for editing your character name. The current names are as follows:\n"
+                                   f"```\n{name_msg}\n```\n"
+                                   f"If you would like to edit another name, repeat the edit command like last time, otherwise type `confirm` to confirm your names.")
 
-    message_state[message.author.id] = 'VALIDATING'
+        message_state[message.author.id] = 'VALIDATING'
+
+    else:
+        character_names = char_name_dict[message.author.id]
+        name_msg = ""
+
+        for char_number in character_names.keys():
+            name_msg += f"{char_number}) {character_names[char_number]}\n"
+
+        await message.channel.send(f"Canceling the name edit command. Your current names are as follows:\n"
+                                   f"```\n{name_msg}\n```\n"
+                                   f"If you would like to edit another name, repeat the edit command like last time, otherwise type `confirm` to confirm your names.")
+        message_state[message.author.id] = 'VALIDATING'
 
 
 async def confirm_names(name_list, message):
@@ -132,7 +164,6 @@ async def confirm_names(name_list, message):
 
 
 async def edit_names(message):
-    # TODO: complete edit command
     split_message = message.content.split()
 
     try:
