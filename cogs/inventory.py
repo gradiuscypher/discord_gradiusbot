ece6783caf1acecc1c1f9a64d55273efcea76954
gradiuscypher <gradiuscypher@gmail.com>
2021-06-17 00:10:56 -0700

    inventory stuff

--- cogs/inventory.py
+++ cogs/inventory.py
@@ -2,12 +2,19 @@
 import discord
 import logging
 import libs.scripts.items
+from libs.db.inventory import ItemManager
 from discord import components
 from discord import embeds
 from discord.ext import commands
 
+# TODO: get_attribute should return value, not object, also need to check use of keepatzero in .remove_item()
+
 # setup logging
 logger = logging.getLogger("gradiusbot")
+
+# message strings
+NO_SUCH_ITEM = "Either you do not have this item, or it does not exist."
+NOT_USABLE = "This item is not usable."
 
 
 class ButtonBuilder(commands.Cog):
@@ -20,12 +27,33 @@ class ButtonBuilder(commands.Cog):
             # method = getattr(libs.scripts.items, 'namecolor')
             # await method(attr_1="Attr1", attr_2=2, interaction=interaction)
             if interaction.data['options'][0]['name'] == 'list':
-                print("list")
-                print(interaction.data)
+                user_inven = ItemManager().get_inventory(interaction.user.id)
+
+                if user_inven:
+                    inven_string = "```\nInventory\n"
+                    for i in user_inven.items:
+                        inven_string += f"\n{i.count} {i.item.name} {i.item.description}"
+                    inven_string += "\n```"
+                    await interaction.response.send_message(inven_string, ephemeral=True)
+                else:
+                    logger.error(f"Failed to create/find inventory for user {interaction.user.id}")
+                    await interaction.response.send_message("Looks like there was an error")
 
             if interaction.data['options'][0]['name'] == 'use':
-                print("use")
-                print(interaction.data)
+                target_item_id = interaction.data['options'][0]['options'][0]['value'].upper()
+                user_inven = ItemManager().get_inventory(interaction.user.id)
+                target_item = user_inven.get_inventory_item(target_item_id)
+
+                if target_item and target_item.count > 0:
+                    usable_item = target_item.item.get_attribute('usable')
+                    if usable_item and bool(usable_item.value):
+                        method = getattr(libs.scripts.items, target_item.item.script)
+                        await method(attr_1="Attr1", attr_2=2, interaction=interaction)
+                    else:
+                        await interaction.response.send_message(NOT_USABLE, ephemeral=True)
+                else:
+                    await interaction.response.send_message(NO_SUCH_ITEM, ephemeral=True)
+
 
 
 
--- libs/db/inventory.py
+++ libs/db/inventory.py
@@ -26,6 +26,31 @@ class ItemManager:
                     session.add(new_attribute)
         session.commit()
 
+    def get_inventory(self, owner_id):
+        """Returns the inventory object of the owner.
+
+        Args:
+            owner_id (Integer): Discord ID of the owner
+
+        Returns:
+            Inventory: The inventory object of the owner, if one doesn't exist, it's created. If error, then None
+        """        
+        inventory_obj = session.query(Inventory).filter(Inventory.owner_id==owner_id).first()
+
+        try:
+            if inventory_obj:
+                return inventory_obj
+            else:
+                new_inventory = Inventory(owner_id=owner_id)
+                logger.info(f"Creating a new inventory for {owner_id}")
+                session.add(new_inventory)
+                session.commit()
+                return new_inventory
+        except:
+            logger.error(traceback.format_exc())
+            return None
+        
+
     def get_item(self, system_name):
         try:
             query = session.query(Item).filter(Item.system_name == system_name)
@@ -37,28 +62,22 @@ class ItemManager:
         except:
             logger.error(traceback.format_exc())
 
-class InventoryManager:
-    def add_item(self, item_id, amount):
-        """Adds a number of items to an inventory
-
-        Args:
-            item_id (Integer): The ID of the item
-            amount (Integer): The number of items you want to add
-        """
-        return True
-
-    def remove_item(self, item_id, amount):
-        return True
-
 
 class Item(Base):
     __tablename__ = 'item'
     id = Column(Integer, primary_key=True)
     attributes = relationship('ItemAttribute')
+    instances = relationship('ItemInstance', backref='item')
     system_name = Column(String)
     name = Column(String)
     description = Column(String)
     script = Column(String)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def get_attribute(self, attribute_name):
+        return session.query(ItemAttribute).filter(ItemAttribute.item_id==self.id, ItemAttribute.name==attribute_name).first()
 
 
 class ItemAttribute(Base):
@@ -73,12 +92,109 @@ class ItemInstance(Base):
     __tablename__ = 'iteminstance'
     id = Column(Integer, primary_key=True)
     item_id = Column(Integer, ForeignKey('item.id'))
-    item = relationship('Item', back_populates="item")
     inventory_id = Column(Integer, ForeignKey('inventory.id'))
+    system_name = Column(String)
+    count = Column(Integer)
 
 
 class Inventory(Base):
     __tablename__ = 'inventory'
     id = Column(Integer, primary_key=True)
-    items = relationship('ItemInstance')
+    items = relationship('ItemInstance', backref='inventory')
+    owner_id = Column(Integer)
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def add_item(self, system_name, amount):
+        """Adds a number of items to an inventory
+
+        Args:
+            system_name (String): The system_name of the item
+            amount (Integer): The number of items you want to add
+
+        Returns:
+            bool: Whether the action succeeded or not
+        """
+        try:
+            target_item = ItemManager().get_item(system_name)
+            logger.debug(f"target_item: {target_item}")
+
+            if target_item:
+                inventory_item = self.get_inventory_item(system_name)
+                logger.debug(f"[add_item] inventory_item: {inventory_item}")
+
+                if inventory_item:
+                    inventory_item.count += amount
+                    session.add(inventory_item)
+                    session.commit()
+                else:
+                    item_instance = ItemInstance(system_name=system_name, item=target_item, inventory_id=self.id, count=amount)
+                    logger.debug(f"item_instance: {item_instance.item.name}, {item_instance.count}")
+                    session.add(item_instance)
+                    session.commit()
+
+                logger.info(f"Added {amount} of {system_name} to {self.owner_id}")
+                return True
+
+            else:
+                logger.error(f"Unable to find item with system_id: {system_name}")
+                return False
+
+        except:
+            logger.error(traceback.format_exc())
+            return False
+
+    def get_inventory_item(self, system_name):
+        """Gets the ItemInstance of an item in the user's inventory
+
+        Args:
+            system_name (String): The Item system_name
+
+        Returns:
+            ItemInstance: Returns an ItemInstance or None
+        """        
+        try:
+            logger.debug(f"[get_inventory_item] - self.id: {self.id} system_name: {system_name}")
+            inventory_item = session.query(ItemInstance).filter(ItemInstance.inventory_id==self.id, ItemInstance.system_name==system_name).first()
+            return inventory_item
+
+        except:
+            logger.error(traceback.format_exc())
+            return None
+
+    def remove_item(self, system_name, amount, dry_run=False):
+        """Removes the provided amount of item_id from the inventory
+
+        Args:
+            system_name (String): The system_name of the item
+            amount (Integer): The amount of the item to remove
+            dry_run (bool, optional): If True, only verifies whether the inventory has at least the amount provided. Defaults to False.
+
+        Returns:
+            bool: Whether the action succeeded or not
+        """        
+        try:
+            inventory_item = self.get_inventory_item(system_name)
+            if inventory_item and inventory_item.count >= amount and amount > 0:
+                if dry_run:
+                    return True
+
+                else:
+                    keep_at_zero = inventory_item.item.get_attribute('keepatzero')
+
+                    if inventory_item.count - amount <= 0 and not keep_at_zero:
+                        session.delete(inventory_item)
+                        session.commit()
+                        return True
+
+                    else:
+                        result = self.add_item(system_name, -amount)
+                        return result
+
+            else:
+                return False
+
+        except:
+            logger.error(traceback.format_exc())
+            return False
