3fc5e9a8317449ac47aed648b69141cdc2c3c1ac
gradiuscypher <gradiuscypher@gmail.com>
2025-11-28 18:39:50 +0000

    scheduled task example

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 A gradiusbot for Discord.
 
 More docs soon, currently re-writing.
+
+# references
+- https://discordpy.readthedocs.io/en/stable/ext/commands/api.html#bot
---
+++ cogs/example_tasks.py
@@ -0,0 +1,24 @@
+from discord.ext import commands, tasks
+
+
+class ExampleTimer(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.index = 0
+        self.printer.start()
+        self.bot = bot
+
+    def cog_unload(self) -> None:
+        self.printer.cancel()
+
+    @tasks.loop(seconds=1.0)
+    async def printer(self) -> None:
+        async for guild in self.bot.fetch_guilds():
+            print(guild)
+
+        print(f"Current example task index: {self.index}")
+        self.index += 1
+
+
+async def setup(bot: commands.Bot) -> None:
+    print("Loading example Cog")
+    await bot.add_cog(ExampleTimer(bot))
